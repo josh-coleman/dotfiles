4998db45dcd87d4ac5627d9689b1f1bee0f8c1ee
Josh Coleman <71773988+josh-coleman@users.noreply.github.com>
2021-09-03 14:48:02 -0500

    Add conditionals

--- setup.sh
+++ setup.sh
@@ -1,10 +1,21 @@
-if [ $SPIN ]; then
-  # Remove old configuration
-  mv ~/.zshrc ~/.zshrc.old
-
-  # Link Zsh config
-  ln -sf ~/dotfiles/.zshrc ~/.zshrc
-
-  # Reload zsh config
-  source ~/.zshrc
+if [ $SPIN ]; 
+then
+  
+  # Core
+  if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]]
+  then
+    'git pull && update && bin/rake dev:create_apps && restart'
+  fi
+  
+  # Web
+  if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/web" ]]
+  then
+    'git pull && update && restart'
+  fi
+  
+  # Shopify Chat
+  if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify_chat" ]]
+  then
+    'git pull'
+  fi
 fi
