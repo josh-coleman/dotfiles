b50a9a71490b980dd04aac9076bc29df0ce914b1
Josh Coleman <71773988+josh-coleman@users.noreply.github.com>
2021-09-03 13:31:56 -0500

    Reset back

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 if [ $SPIN ]; then
-  alias setup_core='git pull && update && bin/rake dev:create_apps && restart'
-  alias setup_web='git pull && update && restart'
+  # Remove old configuration
+  mv ~/.zshrc ~/.zshrc.old
+
+  # Link Zsh config
+  ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+  # Reload zsh config
+  source ~/.zshrc
 fi
