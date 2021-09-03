bebd9ef4a0a5ec1822931aed54d30baff3910d55
Josh Coleman <71773988+josh-coleman@users.noreply.github.com>
2021-09-03 15:51:55 -0500

    Update

--- setup.sh
+++ setup.sh
@@ -4,21 +4,21 @@ then
   # Core
   if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]]
   then
-    'echo Dotfile executing updates for core'
+    echo "Dotfile executing updates for core"
     cd /src/github.com/shopify/shopify && bin/rake dev:create_apps && restart
   fi
   
   # Web
   if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/web" ]]
   then
-    echo Dotfile executing updates for web
-    'git pull && update && restart'
+    echo "Dotfile executing updates for web"
+    git pull && update && restart
   fi
   
   # Shopify Chat
   if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify_chat" ]]
   then
-    'echo Dotfile executing updates for shopify_chat'
-    'git pull'
+    echo "Dotfile executing updates for shopify_chat"
+    git pull
   fi
 fi
